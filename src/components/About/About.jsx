441f15533f8b38423ca4238a9c84c767ebf3a9ea
nihkilrstg18 <nikhilrstg18@gmail.com>
2021-01-27 10:21:13 +0530

    added profile pic updated profile

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -38,15 +38,34 @@ const About = () => {
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
-                  {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                  I'm a <strong>Full Stack Developer</strong>,{' '}
+                  <a href="http://badgecert.com/bc/html/profile.jsp?k=pwywzgg">
+                    <strong>Certified Scrum Master&reg;</strong>
+                  </a>{' '}
+                  and a{' '}
+                  <a href="https://www.certmetrics.com/amazon/public/badge.aspx?i=1&t=c&d=2019-12-02&ci=AWS01115186">
+                    <strong>Certified AWS Solutions Architect - Associate&reg;</strong>
+                  </a>{' '}
+                  living in New Delhi and moving around <strong>NCR, India</strong>.
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                  I’m extremely passionate about web development and design in all it’s forms and
+                  helping <strong>mid and large businesses</strong> and artisans build and improve
+                  their online presence. I spend my work days with my hands in many different areas
+                  of web development from back end programming (<strong>AWS</strong>,{' '}
+                  <strong>C# .Net</strong>, <strong>ASP.NET Core</strong>) to front-end engineering
+                  (<strong>HTML</strong>, <strong>CSS</strong>, <strong>Bootsrap</strong>,{' '}
+                  <strong>Javascript</strong>, <strong>Angular</strong> and <strong>ReactJS</strong>
+                  ), digital accessibility, user experience and visual design.
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+                  Currently I work with{' '}
+                  <a href="https://cygrp.com/">
+                    <strong>Cyber Group</strong>
+                  </a>
+                  . I am not looking for any full time position as of now but I am{' '}
+                  <strong>always open to ideas and projects</strong> that excite me. Aside from web
+                  development,I enjoy spending my time exploring new places and clicking photos.
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
