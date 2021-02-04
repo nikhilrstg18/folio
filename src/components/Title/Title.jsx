bb130fd853d4abca783840d509475655eabb1234
nihkilrstg18 <nikhilrstg18@gmail.com>
2021-02-03 23:03:28 -0600

    improved Title

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -5,7 +5,17 @@ import PropTypes from 'prop-types';
 const Title = ({ title, subTitle }) => (
   <Fade bottom duration={1000} delay={300} distance="0px">
     <h2 className="section-title">{title}</h2>
-    <h4 className="section-subtitle">{subTitle}</h4>
+    {subTitle && (
+      <h4 className="section-subtitle">
+        <span>
+          <i className="fas fa-quote-left" />
+        </span>{' '}
+        {subTitle.toLowerCase()}{' '}
+        <span>
+          <i className="fas fa-quote-right" />
+        </span>
+      </h4>
+    )}
   </Fade>
 );
 
