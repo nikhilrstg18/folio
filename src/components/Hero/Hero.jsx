580d7e55525488136822c7f191704e79d3d4b270
nihkilrstg18 <nikhilrstg18@gmail.com>
2021-03-18 10:23:57 +0530

    added hero animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
+import Hero from '../../images/hero.mp4';
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
@@ -24,6 +25,9 @@ const Header = () => {
   return (
     <section id="hero" className="jumbotron">
       <Container>
+        <video autoPlay loop muted className="hero-video">
+          <source src={Hero} type="video/mp4" />
+        </video>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
